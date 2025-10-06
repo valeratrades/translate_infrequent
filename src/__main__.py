17be43d7dc79ff199b37d62d98ddd2233b2ff8bc
Valera <valeratrades@gmail.com>
2025-10-06 21:51:51 +0000

    style: _

--- src/__main__.py
+++ src/__main__.py
@@ -10,7 +10,7 @@ def create_parser():
 	parser = argparse.ArgumentParser(description="Translate infrequent words in a text to help language learners")
 	parser.add_argument("-l", "--source-lang", type=str, help="Source language")
 	parser.add_argument("-t", "--target-lang", type=str, default="en", help='Target language. DEFAULT: "en"')
-	parser.add_argument("-k", "--known-words", type=int, help="Number of words known (number of most used words we don't translate). DEFAULT: 10_000")
+	parser.add_argument("-w", "--words-n", type=int, help="Number of most used words we don't translate. DEFAULT: 5_000")
 	parser.add_argument("-f", "--file", type=str, help="Input file (default: stdin)")
 	return parser
 
@@ -22,10 +22,10 @@ def main():
 	parser = create_parser()
 	args = parser.parse_args()
 
-	if args.known_words:
-		known_words = args.known_words
+	if args.words_n:
+		words_n = args.words_n
 	else:
-		known_words = 10_000
+		words_n = 5_000
 
 	if args.file:
 		with open(args.file, "r", encoding="utf-8") as f:
@@ -39,7 +39,7 @@ def main():
 	source_lang = Language(args.source_lang)
 	target_lang = Language(args.target_lang)
 
-	out = asyncio.run(src.translate_infrequent(text, source_lang, known_words, target_lang))
+	out = asyncio.run(src.translate_infrequent(text, source_lang, words_n, target_lang))
 	print(out)
 
 
